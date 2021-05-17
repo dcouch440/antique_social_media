934356e39f244cae5a778f29ae44a07b383bb4fc
dcouch440 <dcouch440@gmail.com>
2021-05-17 16:03:02 -0700

    change server location

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const api = require('./src/api');
 const cookieParser = require('cookie-parser');
 const { notFound, handleError } = require('./middleware/exceptions');
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -17,8 +17,7 @@ app.use(notFound);
 app.use(handleError);
 
 api.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
-
 
 module.exports = app;
