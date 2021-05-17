622072c091b37ad73af6fb96910c49d57adf3d95
dcouch440 <dcouch440@gmail.com>
2021-05-17 14:37:27 -0700

    remove netlify

--- netlify-plugin-playtime/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  onBuild: async ({ utils: { build, status, cache, run, git } }) => {
-    await run.command('npm run client-build')
-  },
-  onPostBuild: async ({ utils: { build, status, cache, run, git } }) => {
-    await run.command('npm run client-install');
-    await run.command('npm run server-install');
-  }
-};
