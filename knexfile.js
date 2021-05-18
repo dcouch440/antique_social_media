003fb785fedc15483bd32094fc832eabda5afc06
dcouch440 <dcouch440@gmail.com>
2021-05-17 18:41:08 -0700

    change ssl settings

--- knexfile.js
+++ knexfile.js
@@ -24,14 +24,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + '?sslmode=require',
     pool: {
       min: 2,
       max: 10,
-    },
-    ssl: {
-      sslmode: 'require',
-      rejectUnauthorized: false,
     },
     migrations: {
       directory: './db/migrations',
