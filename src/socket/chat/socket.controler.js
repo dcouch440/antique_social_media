674ebbd37e35d60011bf36ec5d5c067e2ee6266a
dcouch440 <dcouch440@gmail.com>
2021-05-27 12:11:26 -0700

    add controller class and imports

---
+++ src/socket/chat/socket.controler.js
@@ -0,0 +1,9 @@
+const userService = require('../../user/user.service');
+const antiqueService = require('../../antique/antique.service');
+const userSerializer = require('../../user/user.serializer');
+
+class SocketController {
+  async getUsersWithAvatar (req, res) {}
+}
+
+module.exports = new SocketController;
