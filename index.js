e289903f8e8a0d1e1c9a14ac3643212637e180f4
dcouch440 <dcouch440@gmail.com>
2021-05-30 14:24:48 -0700

    remove socket ports

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const app = require('./app');
-const statusSocket = require('./socket/status');
 
 const PORT = process.env.PORT || 3001;
-const STATUS_PORT = 3002;
-
-statusSocket.listen(STATUS_PORT, () => console.log(
-  'socket listening on %d', STATUS_PORT
-));
 
 const server = app.listen(PORT, () => console.log(
   'Updated : Server listening at port %d', PORT
