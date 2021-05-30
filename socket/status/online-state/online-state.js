d9f7913ff5f21da107a0c631e59c05b5251ab141
dcouch440 <dcouch440@gmail.com>
2021-05-30 14:26:22 -0700

    remove unused files

--- socket/status/online-state/online-state.js
+++
@@ -1,5 +0,0 @@
-const createState = require('../../../lib/createState');
-
-const onlineState = createState();
-
-module.exports = onlineState;
--- socket/status/online-state/types.js
+++
@@ -1,7 +0,0 @@
-const USER_ONLINE = 'USER_ONLINE';
-const USER_OFFLINE = 'USER_OFFLINE';
-
-module.exports = {
-  USER_ONLINE,
-  USER_OFFLINE,
-};
