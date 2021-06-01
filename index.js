85f76cc191a542d51143a9c24796bd2546007aa3
dcouch440 <dcouch440@gmail.com>
2021-05-31 18:12:47 -0700

    change socket name to server

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-const server = app.listen(PORT, () => console.log(
-  'Updated : Server listening at port %d', PORT
+app.listen(PORT, () => console.log(
+  'Server listening at port %d', PORT
 ));
-
-module.exports = server;
