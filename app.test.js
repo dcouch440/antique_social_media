ad7d5ea13390711cd14a24bb0ad85662f495d347
dcouch440 <dcouch440@gmail.com>
2021-06-01 22:53:46 -0700

    remove white space

--- app.test.js
+++ app.test.js
@@ -3,9 +3,7 @@ const routesConstants = require('./constant/routes');
 const supertest = require('supertest');
 
 describe('GET /', () => {
-
   it('should respond to with a message', async () => {
-
     const response = await supertest(app)
       .get('/')
       .expect('Content-Type', /json/)
@@ -13,7 +11,5 @@ describe('GET /', () => {
       .catch(err => console.error(err));
 
     expect(response.body.message).toEqual(routesConstants.message);
-
   });
-
 });
