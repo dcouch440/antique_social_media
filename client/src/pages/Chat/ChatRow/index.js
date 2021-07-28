65c9e547c3823c769acb6035206edd37cd2339a2
dcouch440 <dcouch440@gmail.com>
2021-07-28 13:39:29 -0700

    fix bug where offline users could not use back button

--- client/src/pages/Chat/ChatRow/index.js
+++ client/src/pages/Chat/ChatRow/index.js
@@ -29,7 +29,9 @@ export default function ChatRows ({ messages, socketRef }) {
   };
 
   const handleClick = () => {
-    socketRef.current.disconnect();
+    if (socketRef.current) {
+      socketRef.current.disconnect();
+    }
     history.goBack();
   };
 
