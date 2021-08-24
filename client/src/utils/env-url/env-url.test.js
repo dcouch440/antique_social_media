b7dc30a2ddae2411f5f902e38eb3fb985f8d1fe1
dcouch440 <dcouch440@gmail.com>
2021-08-23 17:21:24 -0700

    fix indent

--- client/src/utils/env-url/env-url.test.js
+++ client/src/utils/env-url/env-url.test.js
@@ -5,18 +5,18 @@ describe('envUrl', () => {
     process.env.NODE_ENV = 'production';
     const url = 'https://customUrl.com';
     expect(envUrl({ localhost:3000, production:url }))
-    .toEqual(url);
+      .toEqual(url);
   });
   it('return a non localHostUrl url', () => {
     process.env.NODE_ENV = 'development';
     const url = 'https://customUrl.com';
     expect(envUrl({ localhost:3000, production:url }))
-    .toEqual('http://localhost:3000');
+      .toEqual('http://localhost:3000');
   });
   it('returns localhost if nothing is set', () => {
     process.env.NODE_ENV = null;
     const url = 'https://customUrl.com';
     expect(envUrl({ localhost:3000, production:url }))
-    .toEqual('http://localhost:3000');
+      .toEqual('http://localhost:3000');
   });
 });
