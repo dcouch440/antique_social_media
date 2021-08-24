0807e541085d88ff4218c0fa5f5f74ddacf3b3e5
dcouch440 <dcouch440@gmail.com>
2021-08-23 17:18:22 -0700

    move dropdown styles

--- client/src/Navbar/styles.js
+++ client/src/Navbar/styles.js
@@ -51,3 +51,86 @@ export const Menu = styled.div`
   }
   transition: .1s;
 `;
+
+
+export const DropdownGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-auto-rows: 40px;
+  font-family: 'Pacifico', cursive;
+`;
+
+export const DropdownUsername = styled.div`
+  font-family: 'Pacifico', cursive;
+  background-image: url('https://res.cloudinary.com/dbyretay5/image/upload/v1620696860/pexels-pixabay-210126_eh5kyp.jpg');
+  background-position: end;
+  background-size: cover;
+  filter: grayscale(100%);
+  grid-row: span 3;
+  color: white;
+  text-align: center;
+  border-radius: 6px 6px 0 0;
+  letter-spacing: 3px;
+  border: 1px solid black;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  font-size: 20px;
+  span {
+    background-color: #000000bf;
+    padding: 5px 10px;
+    border-radius: 15px;
+    border: 1px solid white;
+  }
+`;
+
+export const DropdownHubLink = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  letter-spacing: 3px;
+  box-shadow: inset 0px 0px 0px 1px #00000036;
+  :hover {
+    border-color: 2px solid white;
+    a, span {
+      color: white;
+      background-color: #000000ad;
+      padding-left: 30px;
+    }
+  }
+  a, span {
+    cursor: pointer;
+    color: white;
+    font-weight: 300;
+    text-decoration: none;
+    transition: .5s;
+    padding: 5px;
+    padding-left: 20px;
+    width: 100%;
+  }
+`;
+
+export const DropdownBottomLink = styled.div`
+  cursor: pointer;
+  color: white;
+  height: 100%;
+  grid-row: span 2;
+  display: flex;
+  align-items: center;
+  font-family: 18px;
+  padding: 20px;
+  font-weight: 400;
+  border-top: 1px solid black;
+  box-shadow: inset 0px -1px 1px 1px black;
+  transition: .5s;
+  border-radius: 0 0 6px 6px;
+  :hover {
+    color: white;
+    background-color: black;
+    color: white;
+    background-color: #000000ad;
+    padding-left: 30px;
+  }
+`;
