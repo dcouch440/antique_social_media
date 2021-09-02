119f298fdbf310df42d500c4d88d7713355d46b6
dcouch440 <dcouch440@gmail.com>
2021-09-01 23:50:40 -0700

    create urls object

---
+++ client/src/config/index.js
@@ -0,0 +1,10 @@
+export const urls = {
+  chatSocket: {
+    url: 'https://sleepy-beyond-81756.herokuapp.com',
+    localHost: 'http://localhost:3003'
+  },
+  statusSocket: {
+    url: 'https://gentle-brushlands-14865.herokuapp.com',
+    localHost: 'http://localhost:3002'
+  }
+};
