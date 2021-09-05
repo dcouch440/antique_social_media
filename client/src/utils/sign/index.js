a64813a7d1ef70931bb385930c824548b958b88f
dcouch440 <dcouch440@gmail.com>
2021-09-04 20:51:20 -0700

    create sign function

---
+++ client/src/utils/sign/index.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const sign = userCredentials => {
+
+  const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+  const issuer = process.env.REACT_APP_ISS;
+  const subject = process.env.REACT_APP_SUB;
+  const audience = process.env.REACT_APP_AUD;
+  const expiresIn = process.env.REACT_APP_EXP;
+  const algorithm = process.env.REACT_APP_ALG;
+
+  const signOptions = {
+    issuer,
+    subject,
+    audience,
+    expiresIn,
+    algorithm
+  };
+
+  const token = jwt.sign(userCredentials, privateKey, signOptions);
+
+  return token;
+
+};
+
+export default sign;
