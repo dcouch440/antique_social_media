01048c3feaa8195b3f6af725e03579f4672bc102
dcouch440 <dcouch440@gmail.com>
2021-09-04 20:52:37 -0700

    refactore

--- client/src/pages/Antique/DeleteAntique/index.js
+++ client/src/pages/Antique/DeleteAntique/index.js
@@ -10,55 +10,42 @@ import { DeleteButton } from './styles';
 export default function DeleteAntique ({ antique }) {
   const { currentUser } = useContext(Context);
   const history = useHistory();
-  const loading = useRef(false);
+  const requestActive = useRef(false);
 
   const { id, user_id } = antique;
   const isCurrentUsersPost = checkUser({ currentUser, user_id });
 
   const handleDeleteImages = async () => {
-    try {
-      const wasDeleted = await deleteImage();
-      loading.current = false;
-      if (wasDeleted) {
-        history.push('/antiques');
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    const wasDeleted = await deleteImage();
+    requestActive.current = false;
+    if (wasDeleted) { history.push('/antiques'); }
   };
 
   const deleteImage = async () => {
-    try {
-      if (loading.current === true) {
-        return;
-      }
-      loading.current = true;
+    if (requestActive.current === true) { return; }
+    requestActive.current = true;
+    if (!currentUser.admin && !isCurrentUsersPost) { return; }
 
-      if (!currentUser.admin) {
-        if (!isCurrentUsersPost) {
-          return;
-        }
-      }
-      return axios
-        .delete(`/antiques/${id}`, { withCredentials: true })
-        .then(res => (res.status === 204));
-    } catch (err) {
-      console.log(err);
-    }
+    return axios
+      .delete(`/antiques/${id}`,
+        { user_id: currentUser.id },
+        { withCredentials: true }
+      )
+      .then(res => (res.status === 204))
+      .catch(err => console.log(err));
   };
 
   return (
-    isCurrentUsersPost && (
+    isCurrentUsersPost &&
       <DeleteButton onClick={handleDeleteImages}>
         Delete this post
       </DeleteButton>
-    )
   );
 }
 
 DeleteAntique.propTypes = {
   antique: PropTypes.shape({
-    id: PropTypes.string,
+    id: PropTypes.number,
     user_id: PropTypes.number
   })
 };
