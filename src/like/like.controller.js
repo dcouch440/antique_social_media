cf829dd83968594e82a86a41e209992eb4233857
dcouch440 <dcouch440@gmail.com>
2021-09-04 20:56:15 -0700

    refactor

--- src/like/like.controller.js
+++ src/like/like.controller.js
@@ -1,3 +1,4 @@
+const getCurrentUser = require('../../lib/get-current-user');
 const likeSerializer = require('./like.serializer');
 const likeService = require('./like.service');
 
@@ -5,20 +6,24 @@ class LikeController {
   async liked (req,res) {
     try {
       const { antique_id } = req.params;
+      const { user_id } = getCurrentUser(req);
       const isLiked = await likeService.liked({
         antique_id,
-        user_id: req.currentUser.user_id
+        user_id
       });
       res.status(200).json({ isLiked });
     } catch (err) {
       console.error(err);
+      res.status(422).json({ message: err.message });
     }
   }
   async like (req,res) {
     try {
       const { antique_id } = req.params;
+      const { username, user_id } = getCurrentUser(req);
       const liked = await likeService.like({
-        req,
+        username,
+        user_id,
         antique_id
       });
       res.status(201).json(liked);
@@ -30,23 +35,28 @@ class LikeController {
   async unlike (req,res) {
     try {
       const { antique_id } = req.params;
+      const { username, user_id } = getCurrentUser(req);
       const liked = await likeService.unlike({
-        req,
+        username,
+        user_id,
         antique_id
       });
       res.status(204).json(liked);
     } catch (err) {
+      console.error(err);
       res.status(422).json({ message: err.message });
     }
   }
   async likes (req, res) {
     try {
-      const likes = await likeService.likes({ req });
+      const { user_id } = getCurrentUser(req);
+      const likes = await likeService.likes(user_id);
       const likesWithImages = await likeSerializer
         .serializeLikesWithImages(likes);
       res.status(200).json(likesWithImages);
     } catch (err) {
       console.error(err);
+      res.status(422).json({ message: err.message });
     }
   }
 }
