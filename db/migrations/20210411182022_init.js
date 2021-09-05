c59f39bc06e8517578951144f530b54bf9e01f8e
dcouch440 <dcouch440@gmail.com>
2021-09-04 20:54:34 -0700

    refactor

--- db/migrations/20210411182022_init.js
+++ db/migrations/20210411182022_init.js
@@ -1,8 +1,12 @@
 exports.up = async knex => {
   return knex.schema.createTable(
     'user', table => {
-      table.increments();
-      table.string('email').notNullable().unique();
+      table
+        .increments();
+      table
+        .string('email')
+        .notNullable()
+        .unique();
       table.string('username')
         .notNullable()
         .unique();
@@ -10,35 +14,51 @@ exports.up = async knex => {
         .boolean('admin')
         .notNullable()
         .defaultTo(false);
-      table.boolean('online').defaultTo(false);
-      table.string('password_digest').notNullable();
-      table.timestamps(true, true);
-      table.datetime('deleted_at');
+      table
+        .boolean('online')
+        .defaultTo(false);
+      table
+        .string('password_digest')
+        .notNullable();
+      table
+        .timestamps(true, true);
+      table
+        .datetime('deleted_at');
     }
   )
     .createTable(
       'antique', table => {
-        table.increments();
-        table.string('name').notNullable();
-        table.integer('year');
-        table.integer('user_id')
+        table
+          .increments();
+        table
+          .string('name')
+          .notNullable();
+        table
+          .integer('year');
+        table
+          .integer('user_id')
           .unsigned()
           .references('id')
           .inTable('user')
           .onDelete('CASCADE')
           .onUpdate('CASCADE')
           .notNullable();
-        table.string('title')
+        table
+          .string('title')
           .notNullable();
-        table.text('body')
+        table
+          .text('body')
           .notNullable();
-        table.timestamps(true, true);
-        table.datetime('deleted_at');
+        table
+          .timestamps(true, true);
+        table
+          .datetime('deleted_at');
       }
     )
     .createTable(
       'like', table => {
-        table.increments();
+        table
+          .increments();
         table
           .integer('antique_id')
           .unsigned()
@@ -62,9 +82,12 @@ exports.up = async knex => {
           .onDelete('CASCADE')
           .onUpdate('CASCADE')
           .notNullable();
-        table.unique(['antique_id', 'user_id']);
-        table.timestamps(true, true);
-        table.datetime('deleted_at');
+        table
+          .unique(['antique_id', 'user_id']);
+        table
+          .timestamps(true, true);
+        table
+          .datetime('deleted_at');
       }
     );
 };
