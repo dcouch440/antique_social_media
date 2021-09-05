e43bdcbe7fb101913b8c6a8822d68c475408ddab
dcouch440 <dcouch440@gmail.com>
2021-09-04 20:50:52 -0700

    create prevention component

---
+++ client/src/utils/attachImageIfNotPresent/index.js
@@ -0,0 +1,15 @@
+
+// attach avatar if the user comes back from the cloud without an image.
+
+const attachImageIfNotPresent = ({ antique_id }) => {
+  return {
+    safe_url: 'https://res.cloudinary.com/dbyretay5/image/upload/v1621440540/no_image_vbq6q7.png',
+    secure_url: 'http://res.cloudinary.com/dbyretay5/image/upload/v1621440540/no_image_vbq6q7.png',
+    height: '511',
+    width: '1419',
+    antique_id
+  };
+};
+
+
+export default attachImageIfNotPresent;
