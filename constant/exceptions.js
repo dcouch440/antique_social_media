426f073497dc229b5824ade5159cf76e469891f2
dcouch440 <dcouch440@gmail.com>
2021-09-05 18:09:58 -0700

    create exeptions

---
+++ constant/exceptions.js
@@ -0,0 +1,9 @@
+const UNAUTHORIZED = 'Unauthorized';
+const UNIQUE_VIOLATION = 'UniqueViolationError';
+const VALIDATION_ERROR = 'Unauthorized';
+
+module.exports = {
+  UNAUTHORIZED,
+  UNIQUE_VIOLATION,
+  VALIDATION_ERROR
+};
