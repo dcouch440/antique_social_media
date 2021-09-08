6ca850589205d01521d1cbdd083fec492f022cef
dcouch440 <dcouch440@gmail.com>
2021-09-08 07:57:58 -0700

    remove refresher

--- client/src/pages/Chat/index.js
+++ client/src/pages/Chat/index.js
@@ -1,28 +1,20 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
 import { MESSAGE } from '../../constant/index';
 import PageTransition from '../../Framer/PageTransition';
 import useChatSocket from '../../hooks/useChatSocket';
 import ChatInput from './ChatInput';
-import ChatRows from './ChatRow';
+import ChatRows from './ChatRows';
 import OnlineUserSidebar from './OnlineUserSidebar';
 import { ChatWindow } from './styles';
 
 
 export default function Chat ({ roomId }) {
-  const [refresh, setRefresh] = useState(true);
   const { messages, users, socketRef } = useChatSocket(roomId);
 
   const sendMessage = message => {
     socketRef.current.emit(MESSAGE, message);
   };
 
-  useEffect(() => {
-    const refresher = setTimeout(() => {
-      setRefresh(prev => !prev);
-    }, 20000);
-    return () => clearTimeout(refresher);
-  }, [refresh, messages]);
 
   return (
     <PageTransition attr={{ direction: 'bottom' }}>
