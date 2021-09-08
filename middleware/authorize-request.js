7e68288e2f80c36adbb54027b0dd0cf8a1c140f7
dcouch440 <dcouch440@gmail.com>
2021-09-08 08:01:09 -0700

    add notes

--- middleware/authorize-request.js
+++ middleware/authorize-request.js
@@ -1,27 +1,32 @@
 
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const { UNAUTHORIZED } = require('../constant/exceptions');
 
 
 'use strict';
 
 module.exports = function authorizeRequest (req, res, next) {
   try {
-
+    // get user id from user_id header.
     const userIdString = req.headers.user_id;
+    // change value to number to compare the same type.
     const user_id = parseInt(userIdString);
+    // get token from cookies that contains secret user_id.
     const { token } = cookieParser.JSONCookies(req.cookies);
-    const { id: decryptedId } = jwt.verify(
-      token, process.env.JWT_SECRET
-    );
+    // decrypt token user_id and rename
+    const { id: decryptedId } = jwt.verify(token, process.env.JWT_SECRET);
 
+    // if values do not match throw error.
     if (user_id !== decryptedId) {
-      throw new Error('Unauthorized');
+      // halt request and respond with unauthorized.
+      throw new Error(UNAUTHORIZED);
     } else {
-      next();
+      // proceed if checks are met.
+      return next();
     }
 
-  } catch (err) {
-    res.status(401).json({ message: err.message });
+  } catch ({ message }) {
+    res.status(401).json({ message });
   }
 };
