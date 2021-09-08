d0b3a36b40b21174453e8e6b3a64c1ee593859d5
dcouch440 <dcouch440@gmail.com>
2021-09-08 08:03:30 -0700

    refactor

--- src/image/image.service.js
+++ src/image/image.service.js
@@ -1,4 +1,5 @@
 const attachImageIfNotPresent = require('../../lib/attachImageIfNotPresent');
+const ServiceError = require('../../lib/service-error');
 const imageDAO = require('./image.dao');
 
 class ImageService {
@@ -6,37 +7,37 @@ class ImageService {
     try {
       const { resources } = await imageDAO.findByAntiqueId(antique_id);
       return resources;
-    } catch ({ message }) {
-      throw new Error(message);
+    } catch (err) {
+      throw new ServiceError(err);
     }
   }
   async upload ({ file64, antique_id }) {
     try {
       return imageDAO.storeUrl({ file64, antique_id });
-    } catch ({ message }) {
-      throw new Error(message);
+    } catch (err) {
+      throw new ServiceError(err);
     }
   }
   async destroyDependencyById (antique_id) {
     try {
       return imageDAO.destroyAllRelations(antique_id);
-    } catch ({ message }) {
-      throw new Error(message);
+    } catch (err) {
+      throw new ServiceError(err);
     }
   }
   async getFirstImage (antique_id) {
     try {
       const images = await imageDAO.findByIdLimitOne(antique_id);
       return attachImageIfNotPresent({ images, antique_id });
-    } catch ({ message }) {
-      throw new Error(message);
+    } catch (err) {
+      throw new ServiceError(err);
     }
   }
   async getAllImages (antique_id) {
     try {
       return imageDAO.findById(antique_id);
-    } catch ({ message }) {
-      throw new Error(message);
+    } catch (err) {
+      throw new ServiceError(err);
     }
   }
 }
