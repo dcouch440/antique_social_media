81fb07cf51bc3efe15f8d36faa22bf4e07b2f7ec
dcouch440 <dcouch440@gmail.com>
2021-09-08 08:03:59 -0700

    add try catch

--- src/like/like.serializer.js
+++ src/like/like.serializer.js
@@ -1,16 +1,21 @@
+const ServiceError = require('../../lib/service-error');
 const imageService = require('../image/image.service');
 
 class LikesSerializer {
   async serializeLikesWithImages (likes) {
-    const likesWithImages = likes.map(async like => {
-      const { id } = like;
-      const { resources } = await imageService.getFirstImage(id);
-      return {
-        ...like,
-        images: resources
-      };
-    });
-    return Promise.all(likesWithImages);
+    try {
+      const likesWithImages = likes.map(async like => {
+        const { id } = like;
+        const { resources } = await imageService.getFirstImage(id);
+        return {
+          ...like,
+          images: resources
+        };
+      });
+      return Promise.all(likesWithImages);
+    } catch (err) {
+      throw new ServiceError(err);
+    }
   }
 }
 
