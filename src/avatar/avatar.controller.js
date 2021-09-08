7ef1c38f484719098cbb91d8b1825b98c901a119
dcouch440 <dcouch440@gmail.com>
2021-09-08 08:02:41 -0700

    refactor

--- src/avatar/avatar.controller.js
+++ src/avatar/avatar.controller.js
@@ -1,25 +1,23 @@
 const avatarService = require('./avatar.service');
 
 class AvatarController {
-  async destroy (req,res) {
+  async destroy (req, res, next) {
     const { user_id } = req.body;
     try {
       await avatarService.deleteByPublicId(user_id);
       res.status(204).json({ message: 'Deleted' });
     } catch (err) {
-      console.error(err);
-      res.status(422).json({ message: err.message });
+      next(err);
     }
   }
-  async upload (req,res) {
+  async upload (req, res, next) {
     try {
       const { user_id } = req.body;
       const { file64 } = req.body;
       const uploaded = await avatarService.upload({ file64, user_id });
       res.status(201).json(uploaded);
     } catch (err) {
-      console.error(err);
-      res.status(422).json({ message: err.message });
+      next(err);
     }
   }
 }
